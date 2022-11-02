1b197c6f87c7fb9350635eeea02bf2002410d2e0
Work <leonkhartlevi@gmail.com>
2022-11-02 23:43:16 +0700

    create db

---
+++ db/aircrafts_details.sql
@@ -0,0 +1,59 @@
+-- MySQL dump 10.13  Distrib 8.0.31, for Win64 (x86_64)
+--
+-- Host: localhost    Database: aircrafts
+-- ------------------------------------------------------
+-- Server version	8.0.30
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `details`
+--
+
+DROP TABLE IF EXISTS `details`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `details` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `model` varchar(25) NOT NULL,
+  `manufacturers_id` int DEFAULT NULL,
+  `type_id` int DEFAULT NULL,
+  `first_flight` date DEFAULT NULL,
+  `number_built` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `manufacturers_id` (`manufacturers_id`),
+  KEY `type_id` (`type_id`),
+  CONSTRAINT `details_ibfk_1` FOREIGN KEY (`manufacturers_id`) REFERENCES `manufacturers` (`id`),
+  CONSTRAINT `details_ibfk_2` FOREIGN KEY (`type_id`) REFERENCES `type` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `details`
+--
+
+LOCK TABLES `details` WRITE;
+/*!40000 ALTER TABLE `details` DISABLE KEYS */;
+INSERT INTO `details` VALUES (1,'747',2,3,'1969-02-09',1572),(2,'A330',1,3,'1992-11-02',1548),(3,'SSJ100',3,1,'2008-05-19',172),(4,'717',2,2,'1999-10-12',156),(5,'Fokker 100',4,1,'1988-04-03',283),(6,'Tu-154',5,2,'1968-10-04',1026),(7,'737',2,2,'1967-04-09',11154),(8,'A318',1,2,'2002-01-15',80),(9,'DC-8',6,2,'1958-05-30',556),(10,'Challenger 300',7,5,'2004-01-08',450),(11,'Falcon 7X',8,5,'2005-05-05',289);
+/*!40000 ALTER TABLE `details` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-11-02 23:12:55
